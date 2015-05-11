431666346ef7ab66c68ce457e2058afaacf9ea0d
Johannes Lørup Buch <jloerup@gmail.com>
2015-05-11 08:46:05 +0200

    code cleanu and commenting. button for potential white LED was added

--- Cryostage.ino
+++ Cryostage.ino
@@ -1,178 +1,208 @@
-#include <PID_v1.h>
+//Define PID variables
+  #include <PID_v1.h>
+  double Setpoint, Input, Output;
+  PID myPID(&Input, &Output, &Setpoint,5,1,1, REVERSE);
 
-double Setpoint, Input, Output;
+//Define constants and variables for temperature calculation, following Steinhart-Hart.
+  #include <math.h>
+  float T0 = 298.15;
+  float B = 3950;
+  float R0 = 100000;
+  float Ti = 0;
+  float T = 0;
+  float R = 0;
+  float ratio = 0;
 
-PID myPID(&Input, &Output, &Setpoint,5,1,1, REVERSE);
+//Define variables for click state and optional LED
+  #include <Bounce.h>
+  int button = 13;
+  int ledPin = 10;
+  int ledPWM = 0;
+  boolean ledState = LOW;
 
-//Input duty cycle percentage:
-int powerlevel = 0;
+//Define 16x2 LCD
+  #include <LiquidCrystal.h>
+    LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+//Define Adafruit MAX31855 Thermocouple Breakout
+  #include "Adafruit_MAX31855.h"
+  #include <SPI.h>
+  #define DO   6
+  #define CS   7
+  #define CLK  8
+    Adafruit_MAX31855 thermocouple(CLK, CS, DO);
+  double TC = thermocouple.readCelsius();
+  double oldTC;
+
+//Define MOSFET pin
+  int IRL540 = 9;
+
+//Define PWM variable and give initial value
+  double PWM = 10;
 
 //Incoming serial data
-int incoming = 0;
+  int incoming = 0;
 
-//voltage divider input on analog pin 0
-int signal;
-float vout;
-int vin = 5;
+//Voltage divider input on analog pin 0
+  int signal;
+  float vout;
+  int vin = 5;
 
-//define constants and variables for temperature calculation, 
-//following Steinhart-Hart.
+//Define variables used in timekeeping.
+  unsigned long previousTimeLCD = 0;
+  unsigned long currentTimeLCD = 0;
+  int intervalLCD= 500;
 
-float T0 = 298.15;
-float B = 3950;
-float R0 = 100000;
-float Ti = 0;
-float T = 0;
-float R = 0;
-float ratio = 0;
+  unsigned long previousTimeSerial = 0;
+  unsigned long currentTimeSerial = 0;
+  int intervalSerial = 5000;
+  
+  unsigned long previousTimeTC = 0;
+  unsigned long currentTimeTC = 0;
+  int intervalTC = 200;
 
+  //The bouncer refractory period is also deifned in here
+    Bounce bouncer = Bounce( button, 5 );
 
-//for click state
-
-byte state;
-double PWM = 10;
-int button = 13;
-int ledPin = 10;
-
-boolean ledState = LOW;
-
-//initiate pins
-int IRL540 = 9;
-
-#include <Bounce.h>
-#include <SPI.h>
-#include "Adafruit_MAX31855.h"
-
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-#include <math.h>
-
-#define DO   6
-#define CS   7
-#define CLK  8
-Adafruit_MAX31855 thermocouple(CLK, CS, DO);
-
-unsigned long previousTime = 0;
-unsigned long currentTime = 0;
-unsigned long previousTimeSerial = 0;
-unsigned long currentTimeSerial = 0;
-int interval = 500;
-int intervalSerial = 5000;
-Bounce bouncer = Bounce( button, 5 );
-
-double TC = thermocouple.readCelsius();
+/*
+**************************SETUP SETUP SETUP SETUP SETUP****************************
+*/
 
 void setup(){;
-  pinMode(IRL540,OUTPUT);
-  pinMode(button,INPUT);
-  pinMode(ledPin,OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Test");
-  
-  //Init PID control
-  Input = 10;
-  Setpoint = 2;
-  //Turn on PID
-  myPID.SetMode(AUTOMATIC);
-  
-  lcd.begin(16, 2);
-lcd.print("Temperature LCD");
-delay(1000);
 
-lcd.clear();
+  //Define pinmodes for MOSFET, button and optional LED pin
+    pinMode(IRL540,OUTPUT);
+    pinMode(button,INPUT);
+    pinMode(ledPin,OUTPUT);
+    Serial.begin(9600);
+    Serial.println("Test");
+  
+    //Supply temperature readout with an arbitrary initial reading to avoid NaN errors
+    TC = 10;
+    oldTC = TC;
+
+  //Init PID control
+    Input = TC;
+    Setpoint = 2;
+  //Turn on PID
+    myPID.SetMode(AUTOMATIC);
+  
+  //Start the LCD, sey hello and clear
+    lcd.begin(16, 2);
+    lcd.print("Temperature LCD");
+    delay(1000);
+    lcd.clear();
+
+
 
 };
 
-void loop(){;
-
-  //PID Control
-delay(100);
-  TC = thermocouple.readCelsius();
-  Input = TC;
-  myPID.Compute();
-  analogWrite(IRL540,Output);
-  
-
-
-//Receive powerlevel from serial
-
-if (Serial.available() > 0 ){
-		incoming = Serial.parseInt();
-		Setpoint = incoming;
-          //      PWM = map(powerlevel,0,100,0,255);
-          //      lcd.clear();
-}
-
-
 /*
-//Calculate temperature from thermistor connected to A0
+**************************LEWP LEWP LEWP LEWP LEWP LEWP****************************
+*/
+
+void loop()
+{;
+
+  //Buttonpin controls ledPin through ledPWM.
+    if ( bouncer.read() == HIGH) 
+    {
+      ledState = !ledState;
+      digitalWrite( ledPin, ledState );
+    }
+    
+    }
+
+
+  
+analogWrite(IRL540,PWM);
+
+  //PID Control with quality control. NaN will be rejected.
+    currentTimeTC = millis();
+    if ( currentTimeTC - previousTimeTC > intervalTC) 
+    {
+      TC = thermocouple.readCelsius();
+        if (isnan(TC)) 
+        {
+          TC = oldTC;
+        };
+      Input = TC;
+      myPID.Compute();
+      analogWrite(IRL540,Output);
+      //Set oldTC as a real reading for next loop, in case the TC outputs NaN
+        oldTC = TC;
+    }
+
+  //Receive setpoint from serial, if available
+
+   if (Serial.available() > 0 )
+    {
+		  incoming = Serial.parseInt();
+		  Setpoint = incoming;
+    }
+
+  /*
+  //Calculate temperature from thermistor connected to A0
 
     signal = analogRead(A0);
     vout = signal;
     vout = (vout/1023)*vin;
     R = R0*(vin/vout)-R0;
    
-   Ti = (1/T0)+(1/B)*log(R/R0);
-   T = 1/Ti; 
-   T = T - 273.15;
+    Ti = (1/T0)+(1/B)*log(R/R0);
+    T = 1/Ti; 
+    T = T - 273.15;
 */
 
-currentTime = millis();
-currentTimeSerial = millis();
-  
+  //Update the LCD if the intervalLCD time has passed
+    currentTimeLCD = millis();
+    if ( currentTimeLCD - previousTimeLCD > intervalLCD) 
+    {
+    
+      //Cursor is set for first line and TC temperature is printed
+        lcd.setCursor(0,0);
+        lcd.print("TC:");
+        lcd.setCursor(3,0);
+        lcd.print(TC);
+        /*  lcd.setCursor(8,0);
+        lcd.print("TR:");
+        lcd.setCursor(11,0);
+        lcd.print(T);
+        */  
 
+      //Cursor is set for the second line and setpoint + PWM are printed   
+        lcd.setCursor(0,1);
+        lcd.print("SP:");
+        lcd.setCursor(3,1);
+        lcd.print(Setpoint);
+        lcd.setCursor(6,1);
+        lcd.print("C");
+        lcd.setCursor(8, 1);
+        lcd.print("PWM:");
+        lcd.setCursor(12,1);
+        lcd.print(Output);
 
+      //One cycle of LCD update has been executed and the time is set
+        previousTimeLCD = currentTimeLCD ;  
 
-if ( currentTime - previousTime > interval ) {
-  
+    }
+  //Serial communication is on a different timer and is checked like the LCD timer
+    if ( currentTimeSerial - previousTimeSerial > intervalSerial ) 
+    {
+      //Print the incoming data as a confirmation
 
-  
-//Cursor is set for first line
-      
-      
-      lcd.setCursor(0,0);
-      lcd.print("TC:");
-      lcd.setCursor(3,0);
-      lcd.print(TC);
-    /*  lcd.setCursor(8,0);
-      lcd.print("TR:");
-      lcd.setCursor(11,0);
-      lcd.print(T);
-    */  
+		    Serial.print("Received setpoint: ");
+		    Serial.print(Setpoint);
+		    Serial.println(" C");
 
-       
-       lcd.setCursor(0,1);
-       lcd.print("SP:");
-       lcd.setCursor(3,1);
-       lcd.print(Setpoint);
-       lcd.setCursor(6,1);
-       lcd.print("C");
-       lcd.setCursor(8, 1);
-       lcd.print("PWM:");
-       lcd.setCursor(12,1);
-       lcd.print(Output);
-
-previousTime = currentTime;  
-
-}
-if ( currentTimeSerial - previousTimeSerial > intervalSerial ) {
-
-   //Print the incoming data as a confirmation
-
-		Serial.print("Received setpoint: ");
-		Serial.print(Setpoint);
-		Serial.println(" C");
-
-//Print temperature and duty cycle
-          Serial.print("Duty cycle: ");
- 	  Serial.println(PWM);
-
-		Serial.print("Temperature from 100k NTC ");
-		Serial.print(T);
-		Serial.println(" degrees C");   
-      
-       
-
-previousTimeSerial = currentTimeSerial;  
-}
+      //Print temperature and duty cycle
+        Serial.print("Duty cycle: ");
+   	    Serial.println(PWM);
+    		Serial.print("Temperature from 100k NTC ");
+    		Serial.print(T);
+  		  Serial.println(" degrees C");   
+        
+      //Set the Serial time for next cycle
+        previousTimeSerial = currentTimeSerial;  
+   }
 };
