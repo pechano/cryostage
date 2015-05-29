e8781b41dffdf46089999e7aa8cb4a721e9a8d81
Johannes Lørup Buch <jloerup@gmail.com>
2015-05-29 14:34:39 +0200

    29052015. The code is nearing completion. A secondary temperature source has been implemented in the form of a 100k NTC on pin A0. The pot has been moved to A2. The Adafruit MAX31855 library will probably be replaced soon because of the heavy license conditions that MUST be stated

--- Cryostage.ino
+++ Cryostage.ino
@@ -1,15 +1,18 @@
 //****************************LIBRARIES****************************
 
-    #include "max6675.h" //Used for the MAX6675 breakout board from adafruit. https://github.com/adafruit/MAX6675-library
+
+    #include "SPI.h" //SPI Library supplied with the Arduino. A dependency of the Adafruit MAX31855 library.
+    #include "Adafruit_MAX31855.h" //Used for communicating with the MAX31855 chip. https://github.com/adafruit/Adafruit-MAX31855-library
     #include <PID_v1.h> //The Arduino PID algorithm by Brett Beauregaard, used to control temperature in setpoint mode. https://github.com/br3ttb/Arduino-PID-Library
-    #include <math.h> //Math functions used in thermistor calculations. Can perhaps be excluded in future versions
+    #include <math.h> //Math functions used in thermistor calculations.
     #include <Wire.h> //I2C library used for communicating with the LCD.
     #include "rgb_lcd.h" //Grove LCD RGB Backlight library. https://github.com/Seeed-Studio/Grove_LCD_RGB_Backlight
 
 
 //****************************PINS*********************************
 
-//Initiate Adafruit MAX6675 Thermocouple Breakout pins. SPI Device, with the protocol embedded in the max6675 library from adafruit.
+
+//Initiate Adafruit MAX31855 Thermocouple Breakout pins. SPI Device.
     int thermoCLK = 5;
     int thermoCS = 4;
     int thermoDO = 3;
@@ -31,11 +34,12 @@
 
 
 // Initialize the Thermocouple
-    MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+    Adafruit_MAX31855 thermocouple(thermoCLK, thermoCS, thermoDO);
 
  
 
 //************************VARIABLES******************************
+
 
 //Define PID variables
     double Setpoint, Input, Output;
@@ -43,12 +47,16 @@
 
 //Define constants and variables for temperature calculation, following Steinhart-Hart.
     float T0 = 298.15;
-    float B = 3950;
-    float R0 = 100000;
+    float B = 3950;//B-value from the datasheet of the 100k NTC.
+    float R0 = 101050;//Measured on 100k resistor with multimeter.
     float Ti = 0;
     float T = 0;
     float R = 0;
-    float ratio = 0;
+
+//Variables needed for voltage divider at the 100k NTC on A2.
+    int signal;
+    float vout;
+    float vin = 4.97;//Measured between 5v and GND on the Arduino, using a multimeter.
 
 //Initiate the tempstate variable which decides the mode of action for temperature control. The initial value "0" corresponds to load (PWM=0).    
     int tempstate = 0;
@@ -66,12 +74,10 @@
     double oldTC = 10.0;
 
 //Variable used to store incoming serial data. The stored value can be used as setpoint for the PID control
-    int incoming = 10;
+    float incoming = 10.0;
 
-//Voltage divider input on analog pin 0
-    int signal;
-    float vout;
-    int vin = 5;
+//Voltage divider input on analog pin 2. 
+    int potSignal;
 
 //Define variables used in timekeeping.
     unsigned long previousTimeLCD = 0;
@@ -84,7 +90,7 @@
   
     unsigned long previousTimeTC = 0;
     unsigned long currentTimeTC = 0;
-    int intervalTC = 100;
+    int intervalTC = 300;
 
 //**************************SETUP SETUP SETUP SETUP SETUP****************************
 
@@ -139,13 +145,24 @@ void setup(){
 */
 
 void loop(){
+    
+     //Calculate temperature from thermistor connected to A0
 
+        signal = analogRead(A0);
+        vout = signal;
+        vout = (vout/1023)*vin;
+        R = R0*(vin/vout)-R0;
+   
+        Ti = (1/T0)+(1/B)*log(R/R0);
+        T = 1/Ti; 
+        T = T - 273.15;
+        
+      
     //If intervalTC time has passed, a readout will be stored in the TC variable. NaN will result in an extra reading before finally using oldTC if the problem persists
 
         currentTimeTC = millis();
         if  (currentTimeTC - previousTimeTC > intervalTC) {
             TC = thermocouple.readCelsius();
-            Serial.println(TC);
             if  (isnan(TC)) {
                 TC = thermocouple.readCelsius();
                 if (isnan(TC)) {
@@ -199,7 +216,7 @@ void loop(){
                 digitalWrite(PIDLED, LOW);
                 digitalWrite(freezeLED, LOW);
                 digitalWrite(microscopeLED,LOW);
-                colorR = 255;colorG=0;colorB=0;
+                colorR = 0;colorG=255;colorB=0;
                 lcd.setRGB(colorR, colorG, colorB);
             break;
 
@@ -207,10 +224,10 @@ void loop(){
                 digitalWrite(PIDLED,HIGH);
                 digitalWrite(freezeLED,LOW);
                 digitalWrite(microscopeLED,HIGH);
-                colorR=255-int(Output);colorG=0;colorB=int(Output);
+                colorR=255-int(Output);colorG=0;colorB=int(Output);//LCD backlight is set according to cooling power. 255=blue, 0=red.
                 lcd.setRGB(colorR, colorG, colorB);
             
-                //PID Control with quality control. NaN will be rejected.
+                //PID Control with quality control, using the TC value from the beginning of the loop.
                     Input = TC;
                     myPID.Compute();
                     analogWrite(IRL540,Output);
@@ -229,10 +246,10 @@ void loop(){
             break;
 
             case 3://Tempstate 3 is the same as load, but with the microscope LED also turned on. The LCD backlight is also set to full white, so this loading mode provides some work light.
-                signal = analogRead(A0);
-                signal = map(signal,0,925,0,255);//925 was chosed instead of the usual 1023 because the voltage on the 5v rail was 4.5v in this particular case.
-                signal = constrain(signal,0,255);        
-                Output = signal;
+                potSignal = analogRead(A2);
+                potSignal = map(potSignal,0,925,0,255);//925 was chosed instead of the usual 1023 because the voltage on the 5v rail was 4.5v in this particular case.
+                potSignal = constrain(potSignal,0,255);        
+                Output = potSignal;
                 analogWrite(IRL540,Output);
                 digitalWrite(PIDLED,LOW);
                 digitalWrite(freezeLED, LOW);
@@ -253,19 +270,8 @@ void loop(){
         }
 
 
-//The following section was commented out since the move to a thermocouple based solution.
-  /*
-  //Calculate temperature from thermistor connected to A0
 
-    signal = analogRead(A0);
-    vout = signal;
-    vout = (vout/1023)*vin;
-    R = R0*(vin/vout)-R0;
-   
-    Ti = (1/T0)+(1/B)*log(R/R0);
-    T = 1/Ti; 
-    T = T - 273.15;
-*/
+ 
 
     //Update the LCD if the intervalLCD time has passed
         
@@ -295,7 +301,7 @@ void loop(){
                 lcd.setCursor(12,1);
                 lcd.print(Output);
 
-            //One cycle of LCD update has been executed and the time is set
+            //One cycle of LCD update has been executed and the timer is reset
                 previousTimeLCD = currentTimeLCD ;  
 
         }
@@ -314,8 +320,10 @@ void loop(){
        	        Serial.println(Output);
       	 	    Serial.println(" C");   
                 Serial.println(TC);
+                Serial.print("100kNTC : ");
+                Serial.println(T);
                 
-            //Set the Serial time for next cycle
+            //Reset the Serial timer for next cycle
                 previousTimeSerial = currentTimeSerial;  
         }
 }
